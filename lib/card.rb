bbe396b8fb31849ffaf59a84904f0c4354c8ecfd
Luis Senior <heybluguy@yahoo.com>
2017-09-12 01:23:40 -0600

    iteration 2

--- lib/card.rb
+++ lib/card.rb
@@ -16,22 +16,24 @@ class Card
 end
 
 class Guess
+  attr_reader :card, :response
   def initialize(response, card)
     @response = response
     @card = card
     @correct = false
   end
 
-  def card
-    @card
-  end
-
-  def response
-    @response
-  end
-
   def correct?
     @correct = true if @response == @card.answer
+    @correct
+  end
+
+  def feedback
+    if @correct == true
+      "Correct!"
+    else
+      "Incorrect."
+    end
   end
 
 end
