175121fef4311431d7146948030eaa50a4613b96
Luis Senior <heybluguy@yahoo.com>
2017-09-12 11:38:44 -0600

    iteration 3 tests

--- test/card_test.rb
+++ test/card_test.rb
@@ -4,18 +4,36 @@ require 'minitest/pride'
 require './lib/card'
 
 class CardsTest < Minitest::Test
-  def test_cards_have_questions
+  def test_card_one_has_a_question
     card = Card.new("What is the capital of Alaskan?", "Juneau")
       expected = "What is the capital of Alaskan?"
 
       assert_equal expected, card.question
   end
 
-  def test_cards_have_answers
+  def test_card_one_has_an_answer
     card = Card.new("What is the capital of Alaskan?", "Juneau")
       expected = "Juneau"
 
       assert_equal expected, card.answer
+  end
+
+  def test_cards_stored_inside_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+    deck = Deck.new([card_1, card_2, card_3])
+
+    assert_equal [card_1, card_2, card_3], deck.cards
+  end
+
+  def test_count_the_cards_stored_inside_deck
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars")
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west")
+    deck = Deck.new([card_1, card_2, card_3])
+
+    assert_equal 3, deck.count
   end
 end
 
@@ -71,13 +89,10 @@ class GuessTest < Minitest::Test
   end
 
   def test_gives_feedback_for_false_response
-    card = Card.new("Which planet is closest to the sun?", "Mercury")
-    guess = Guess.new("Saturn", card)
+    card_1 = Card.new("Which planet is closest to the sun?", "Mercury")
+    guess = Guess.new("Saturn", card_1)
     guess.correct?
 
     assert_equal "Incorrect.", guess.feedback
   end
-
-
-
 end
