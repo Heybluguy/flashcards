b9761deeb012a35b3955f72cf7f944245cefb5aa
Luis Senior <heybluguy@yahoo.com>
2017-09-12 01:24:21 -0600

    iteration 2

--- test/card_test.rb
+++ test/card_test.rb
@@ -31,7 +31,7 @@ class GuessTest < Minitest::Test
     card = Card.new("What is the capital of Alaskan?", "Juneau")
     guess = Guess.new("Juneau", card)
 
-    assert guess.response
+    assert_equal "Juneau", guess.response
   end
 
   def test_check_if_response_is_correct
@@ -41,8 +41,43 @@ class GuessTest < Minitest::Test
     assert guess.correct?
   end
 
+  def test_gives_feedback
+    card = Card.new("What is the capital of Alaskan?", "Juneau")
+    guess = Guess.new("Juneau", card)
+    guess.correct?
 
-
+    assert_equal "Correct!", guess.feedback
   end
+
+  def test_guess_takes_card_argument
+    card = Card.new("Which planet is closest to the sun?", "Mercury")
+    guess = Guess.new("Saturn", card)
+
+    assert_equal card, guess.card
+  end
+
+  def test_receives_response_for_a_guess
+    card = Card.new("Which planet is closest to the sun?", "Mercury")
+    guess = Guess.new("Saturn", card)
+
+    assert_equal "Saturn", guess.response
+  end
+
+  def test_check_if_response_is_false
+    card = Card.new("Which planet is closest to the sun?", "Mercury")
+    guess = Guess.new("Saturn", card)
+
+    refute guess.correct?
+  end
+
+  def test_gives_feedback_for_false_response
+    card = Card.new("Which planet is closest to the sun?", "Mercury")
+    guess = Guess.new("Saturn", card)
+    guess.correct?
+
+    assert_equal "Incorrect.", guess.feedback
+  end
+
+
 
 end
