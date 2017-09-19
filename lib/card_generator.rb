17f1a0cb2ece028f0bfd124578c432ac74fd481b
Luis Senior <heybluguy@yahoo.com>
2017-09-18 22:23:29 -0600

    Iteration 6 almost done

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,5 +1,17 @@
+require "./lib/card"
+
 class CardGenerator
+  attr_reader :file
   def initialize(filename)
-    @file = filename
+    @file = File.read(filename)
+    @deck = []
+  end
+
+  def cards
+    cards = @file.split("\n").map {|card| card.split(",")}
+    cards.each do |card|
+      @deck << Card.new(card[0], card[1])
+    end
+    @deck
   end
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -2,12 +2,14 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card_generator'
 require './lib/card'
+require 'pry'
 
 class CardGeneratorTest < MiniTest::Test
   def test_cards_exist
     filename = "cards.txt"
     cards = CardGenerator.new(filename).cards
-    expected = 0
-    assert_equal expected, cards
+
+    assert_instance_of Array, cards
+    assert_instance_of Card, cards.first
    end
  end
